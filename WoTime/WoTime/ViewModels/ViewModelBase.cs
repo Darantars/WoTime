8da5e81ea93ab342425a8ead3c6b75fd5484f342
Darantar <Darantar@DESKTOP-EK9VSKR>
2024-08-25 23:16:37 +0700

    Main frame

---
+++ WoTime/WoTime/ViewModels/ViewModelBase.cs
@@ -0,0 +1,8 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+
+namespace WoTime.ViewModels
+{
+    public abstract class ViewModelBase : ObservableObject
+    {
+    }
+}
--- WoTime_Desctop/ViewModels/ViewModelBase.cs
+++
@@ -1,8 +0,0 @@
-﻿using CommunityToolkit.Mvvm.ComponentModel;
-
-namespace WoTime_Desctop.ViewModels
-{
-    public class ViewModelBase : ObservableObject
-    {
-    }
-}
